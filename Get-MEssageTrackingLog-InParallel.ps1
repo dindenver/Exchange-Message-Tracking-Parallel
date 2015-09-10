7f3f6c65960469163bd83c89bf904802aec8a0e1
Dave M <dindenver@yahoo.com>
2015-09-10 15:26:08 -0600

    Update Get-MEssageTrackingLog-InParallel.ps1
    Fixed an issue with variable assignment across the InlineScript scope.

--- Get-MEssageTrackingLog-InParallel.ps1
+++ Get-MEssageTrackingLog-InParallel.ps1
@@ -41,12 +41,15 @@ $GUI,
         [Alias("Name")]
 $Filename="$($pwd.path)`\MessageTracking-$(GC ENV:Username)-$(get-date -format MMddyy).CSV"
 )
-
+write-verbose "Initializing..."
 # Initialization
 # Does not truncate output
 $FormatEnumerationLimit=-1
+write-verbose "`$FormatEnumerationLimit set"
 [String[]]$EXServers=@()
+write-verbose "`$EXServers initilized"
 
+write-verbose "Loading Workflow into memory"
 Workflow Main
 	{
 	param (
@@ -64,11 +67,12 @@ Workflow Main
 	$Subject
 	)
 # Search message tracking logs on each server.
+write-verbose "Starting ForEach Loop"
 	foreach -parallel ($EXServer IN $EXServers)
 		{
-		[array]$exdata += InlineScript
+		InlineScript
 			{
-			$isdata=@()
+write-verbose "$Using:EXServer - Loading Function Track-ExMessage"
 			Function Track-ExMessage
 				{
 				param (
@@ -85,49 +89,55 @@ Workflow Main
 					[String]
 				$Subject
 				)
-				$fndata=@()
-write-verbose "Processing $EXServer"
+write-verbose "$EXServer - Processing $EXServer"
 # $Sender has to be $null if it is not specified "" only matches blank sender, $null matches all senders
 				if ($Sender -eq "") {Remove-Variable Sender;$Sender = $null}
+write-verbose "$Using:EXServer - `$Sender Value: $Sender"
 # $Recipient has to be $null if it is not specified "" only matches blank Recipient, $null matches all Recipients
 				if ($Recipient -eq "") {Remove-Variable Recipient;$Recipient = $null}
+write-verbose "$EXServer - `$Recipient Value: $Recipient"
 # $Subject has to be $null if it is not specified "" only matches blank Subject, $null matches all Subjects
 				if ($Subject -eq "") {Remove-Variable Subject;$Subject = $null}
-				Add-PSSnapin Microsoft.Exchange.Management.PowerShell.E2010
-# Loads Remote Management tools
-#	if (test-path 'C:\Program Files\Microsoft\Exchange Server\V14\bin\Exchange.ps1') {. 'C:\Program Files\Microsoft\Exchange Server\V14\bin\Exchange.ps1' -erroraction silentlyContinue | out-null}
-# 	if (test-path 'C:\Program Files\Microsoft\Exchange Server\V14\bin\RemoteExchange.ps1') {. 'C:\Program Files\Microsoft\Exchange Server\V14\bin\RemoteExchange.ps1' -erroraction silentlyContinue | out-null;Connect-ExchangeServer -AllowClobber -ServerFqdn $EXServer | out-null}
-write-verbose "Tracking on $($EXServer)"
-				$fndata+=Get-MessageTrackingLog -Server $EXServer -Sender $Sender -Recipient $Recipient -Start $Start -End $End -MessageSubject $Subject -ResultSize Unlimited
-write-verbose "Tracking on $($EXServer) successful - $?"
-write-verbose "$($EXServer) Messages found: $($fndata.count)"
-				Return $fndata
-			} # Function Track-ExMessage
+write-verbose "$EXServer - `$Subject Value: $Subject"
 
-			$isdata += Track-ExMessage -EXServer $Using:EXServer -Sender $Using:Sender -Recipient $Using:Recipient -Start $Using:Start -End $Using:End -Subject $Using:Subject
-write-verbose "$($Using:EXServer) Message count: $($isdata.count)"
-			$isdata
+write-verbose "$EXServer - Loading Snap In"
+				Add-PSSnapin Microsoft.Exchange.Management.PowerShell.E2010
+write-verbose "$EXServer - Snap In Loaded successfully: $?"
+write-verbose "Tracking on $($EXServer)"
+				Get-MessageTrackingLog -Server $EXServer -Sender $Sender -Recipient $Recipient -Start $Start -End $End -MessageSubject $Subject -ResultSize Unlimited
+write-verbose "Tracking on $($EXServer) successful - $?"
+			} # Function Track-ExMessage
+write-verbose "$Using:EXServer - Function Loaded..."
+
+write-verbose "$Using:EXServer - Executing Function Track-ExMessage -EXServer $Using:EXServer -Sender $Using:Sender -Recipient $Using:Recipient -Start $Using:Start -End $Using:End -Subject $Using:Subject"
+			Track-ExMessage -EXServer $Using:EXServer -Sender $Using:Sender -Recipient $Using:Recipient -Start $Using:Start -End $Using:End -Subject $Using:Subject
 			} # InlineScript
 		} # foreach -parallel ($EXServer IN $EXServers)
-write-verbose "Total Message count: $($exdata.count)"
-	return $exdata
 } # Workflow Main
+write-verbose "Workflow Loaded"
 
 # Makes an array of the internal Transport servers
+write-verbose "Collecting servers"
 Get-TransportServer | where {(test-connection $_.name -count 1 -quiet -erroraction silentlycontinue) -eq $true} | sort name | foreach {$EXServers+=$_.Name}
 write-verbose "Server List: $EXServers"
 write-verbose "Exchange Transport Seervers collected successfully: $? - Servers collected: $($EXServers.count)"
 
 # Capture the output from Main
+write-verbose "Running Main -EXServers $EXServers -Sender $Sender -Recipient $Recipient -Start $Start -End $End -Subject $Subject"
 $trackingdata = Main -EXServers $EXServers -Sender $Sender -Recipient $Recipient -Start $Start -End $End -Subject $Subject
+write-verbose "$($trackingdata.count) event found."
 
 # Sort by TimeStamp and only select the fields we want
+write-verbose "Sorting results"
 $trackingdata = $trackingdata | sort Timestamp | Select "Timestamp","ClientIp","ClientHostname","ServerIp","ServerHostname","ConnectorId","Source","EventId","InternalMessageId","MessageId","Recipients","RecipientStatus","TotalBytes","RecipientCount","RelatedRecipientAddress","MessageSubject","Sender","ReturnPath","MessageInfo","MessageLatency","MessageLatencyType","EventData"
+write-verbose "Results sorted"
 
 # return the data differently based on what parameters were used
+write-verbose "Determing requested output."
 switch ($PsCmdlet.ParameterSetName)
     {
     "PSO"   { return $trackingdata; break}
     "GUI"   { $trackingdata | Out-GridView -Title "Message Tracking Summary"; break}
     default { $trackingdata  | Export-Csv -Path $FileName -NoTypeInformation -Force -Confirm:$false; break}
     } 
+write-verbose "Data output"
